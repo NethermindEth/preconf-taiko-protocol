6bf3eced18cc2d3add45a6cf66ed57f7252b380c
mikhailUshakoff <mikhail.ushakov@yahoo.com>
2025-10-01 08:20:53 +0000

    remove randomnessDelay patch

--- setup.sh
+++ setup.sh
@@ -26,8 +26,6 @@ sed -i "s|^[[:space:]]*uint256 internal constant SECONDS_IN_SLOT = 12;|    uint2
 
 sed -i "s|^[[:space:]]*uint256 internal constant ETHEREUM_BLOCK_TIME = 12 seconds;|    uint256 internal constant ETHEREUM_BLOCK_TIME = ${DEVNET_SECONDS_IN_SLOT} seconds;|" "$NETWORK_FILE"
 
-# --- Patch deploy script to set Whitelist's operatorChangeDelay and randomnessDelay
-sed -i "s|^[[:space:]]*data: abi.encodeCall(PreconfWhitelist.init, (owner, 2, 2))|    data: abi.encodeCall(PreconfWhitelist.init, (owner, ${DEVNET_OP_CHANGE_DELAY}, ${DEVNET_RANDOMNESS_DELAY}))|" "$DEPLOYMENT_SCRIPT_FILE"
 # --- Patch deploy script remove addOperator call
 sed -i "s|^[[:space:]]*PreconfWhitelist(whitelist).addOperator(proposer, proposer);| |" "$DEPLOYMENT_SCRIPT_FILE"
 
